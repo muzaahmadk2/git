ce9fa0af6f17201d9ef06463a0795ef136936604
muzaahmadk2 <muzaahmadk2@gmail.com>
2022-11-22 22:16:56 +0400

    main code

---
+++ main1.js
@@ -0,0 +1,11 @@
+//document.title ='234';
+//console.log(document.tilte);
+//console.log(document.all[10]);
+
+var headertitle = document.getElementById('header-title');
+headertitle.style.borderBottom = 'solid 3px black';
+var item = document.getElementsByClassName('title');
+item[0].style.fontWeight ='bold';
+item[0].style.color='green';
+
+
