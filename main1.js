6674bd38d2496ae0c4593d2f83f2df974ceaf3b6
muzaahmadk2 <muzaahmadk2@gmail.com>
2022-11-22 22:41:54 +0400

    bg change

--- main1.js
+++ main1.js
@@ -1,11 +1,6 @@
-//document.title ='234';
-//console.log(document.tilte);
-//console.log(document.all[10]);
-
-var headertitle = document.getElementById('header-title');
-headertitle.style.borderBottom = 'solid 3px black';
-var item = document.getElementsByClassName('title');
-item[0].style.fontWeight ='bold';
-item[0].style.color='green';
-
-
+var li =document.getElementsByClassName('list-group-item');
+li[2].style.backgroundColor = 'green';
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.fontWeight = 'bold';
+}
