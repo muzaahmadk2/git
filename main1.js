652f2309be522250cc5fbbb4f11106f2313c2e19
muzaahmadk2 <muzaahmadk2@gmail.com>
2022-11-22 22:53:30 +0400

    new li without same class

--- main1.js
+++ main1.js
@@ -1,6 +1,13 @@
-var li =document.getElementsByClassName('list-group-item');
-li[2].style.backgroundColor = 'green';
+//var li =document.getElementsByClassName('list-group-item');
+//li[1].textContent = 'hello';
+//for(var i=0;i<li.length;i++)
+//{
+    //i[i].style.backgroundColor = '#f4f4f4';
+//}
+
+var li =document.getElementsByTagName('li');
+li[1].textContent = 'hello';
 for(var i=0;i<li.length;i++)
 {
-    li[i].style.fontWeight = 'bold';
+    li[i].style.backgroundColor = '#f4f4f4';
 }
