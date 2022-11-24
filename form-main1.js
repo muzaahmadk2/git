111079fac57497167e2a658d0bcc559d605e8fbb
muzaahmadk2 <118838133+muzaahmadk2@users.noreply.github.com>
2022-11-24 23:59:38 +0400

    multiple user

--- form-main1.js
+++ form-main1.js
@@ -4,7 +4,18 @@ const myForm = document.querySelector('#my-form');
   const msg = document.querySelector('.msg');
   const userList = document.querySelector('#users');
   
-  
+                       //show list of users
+
+  let k =JSON.parse(localStorage.getItem('userdetails'));
+
+  if(k){
+    k.forEach(element => {
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(`${element.name}: ${element.email}`));
+      userList.appendChild(li);
+    });
+  }
+
   myForm.addEventListener('submit', onSubmit);
   
   function onSubmit(e) {
@@ -18,29 +29,28 @@ const myForm = document.querySelector('#my-form');
         setTimeout(() => msg.remove(), 3000);
       } else {
         
+        
+
+                                   // add multiple details to local storage
+
+        let item = localStorage.getItem('userdetails') || '[]';
+        item = JSON.parse(item);
+        item.push({name: nameInput.value, email: emailInput.value});
+        localStorage.setItem('userdetails', JSON.stringify(item));
+
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
         userList.appendChild(li);
 
-        // add to local storage
 
-        // localStorage.setItem('user_name' , nameInput.value);
-        // localStorage.setItem('user_email' , emailInput.value);
-
-        const obj = {};
-        obj.name = nameInput.value;
-        obj.email = emailInput.value;
-
-        let userobj = JSON.stringify(obj);
-        localStorage.setItem('user' , userobj);
-
-    
-       
         nameInput.value = '';
         emailInput.value = '';
          //
       }
     }
+    
+  
+    
     
   
     
