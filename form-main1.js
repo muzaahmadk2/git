0059be2f0351b78fcff46c60c72394f0a5dc37f8
muzaahmadk2 <118838133+muzaahmadk2@users.noreply.github.com>
2022-11-27 15:13:13 +0400

    added edit and delete button

--- form-main1.js
+++ form-main1.js
@@ -10,9 +10,7 @@ const myForm = document.querySelector('#my-form');
 
   if(k){
     k.forEach(element => {
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(`${element.name}: ${element.email}`));
-      userList.appendChild(li);
+      addele(element);
     });
   }
 
@@ -38,9 +36,12 @@ const myForm = document.querySelector('#my-form');
         item.push({name: nameInput.value, email: emailInput.value});
         localStorage.setItem('userdetails', JSON.stringify(item));
 
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-        userList.appendChild(li);
+        var ele ={
+          name:nameInput.value,
+          email:emailInput.value
+        };
+        addele(ele);
+
 
 
         nameInput.value = '';
@@ -48,9 +49,81 @@ const myForm = document.querySelector('#my-form');
          //
       }
     }
+
+    // function to delete item
+    userList.addEventListener('click' , remove);
+    function remove(e){
+      if(e.target.classList.contains('delete')){
+        
+          var li = e.target.parentElement;
+          var ri = li.childNodes[1].textContent
+          userList.removeChild(li);
+
+          let k =JSON.parse(localStorage.getItem('userdetails'));
+          if(k){
+            k = k.filter(element => {
+              return element.email != ri;
+                
+            });
     
-  
+          }
+          localStorage.setItem('userdetails', JSON.stringify(k));
+        }
+      }
+
+    //function to edit item
+    userList.addEventListener('click' , remove1);
+    function remove1(e){
+      if(e.target.classList.contains('edit')){
+
+        
+          var li = e.target.parentElement;
+          var ri = li.childNodes[1].textContent
+          userList.removeChild(li);
+
+          let k =JSON.parse(localStorage.getItem('userdetails'));
+          if(k){
+            k = k.filter(element => {
+              if(element.email == ri){
+                nameInput.value = element.name;
+                emailInput.value = element.email;
+              }
+              return element.email != ri;
+                
+            });
     
+          }
+          localStorage.setItem('userdetails', JSON.stringify(k));
+        }
+      }
+
+
+
+    function addele(element){
+      const li = document.createElement('li');
+      li.appendChild(document.createTextNode(`${element.name}: `));
+      li.appendChild(document.createTextNode(element.email));
+
+      // add edit button;
+      var ebutt = document.createElement('button');
+      ebutt.className = 'edit';
+      ebutt.appendChild(document.createTextNode('Edit'));
+      ebutt.style.marginLeft = '10px';
+      li.appendChild(ebutt);
+
+      //add delete button
+      var dbutt = document.createElement('button');
+      dbutt.className = 'delete';
+      dbutt.appendChild(document.createTextNode('Delete'));
+      dbutt.style.marginLeft = '10px';
+      li.appendChild(dbutt);
+
+
+      userList.appendChild(li);
+
+    }
+
+
     
   
     
